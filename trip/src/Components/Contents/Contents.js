ae27c481a133eb4525e096a641a2cba90be6caa7
MoonHo <75512752+monho@users.noreply.github.com>
2022-06-28 13:01:14 +0900

    [edit]Edit to Contents.js
    [edit]Edit to Contents.js

--- trip/src/Components/Contents/Contents.js
+++ trip/src/Components/Contents/Contents.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React from 'react'; // import 로 useState 를 불러온다!
 import styled from 'styled-components';
 
-const Contentarea = styled.div`
-    width: 100%;
-`
 
-function Contents(){
-    return
-    <Contentarea>
-        
-    </Contentarea>
+
+
+const Contents = () => {
+    
+    const MainSection = styled.section`
+    margin: 0px;
+    padding:0px;
+    
+`; //ContentWarp 메인 css
+
+    return(
+        <MainSection>
+            
+        </MainSection>
+    )
 
 }
-  
-
 
 export default Contents;
