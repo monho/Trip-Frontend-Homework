fadc4df3c71962e499850eead59ad9858e12e0fe
MoonHo <75512752+monho@users.noreply.github.com>
2022-06-28 12:56:42 +0900

    [add]Fabicon.ico
    [add]Fabicon.ico

---
+++ trip/src/Components/Contents/Contents.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from 'styled-components';
+
+const Contentarea = styled.div`
+    width: 100%;
+`
+
+function Contents(){
+    return
+    <Contentarea>
+        
+    </Contentarea>
+
+}
+  
+
+
+export default Contents;
